14325f24cffed0a594527ba33fd0188408c225f9
Diogo Oliveira <dluks82@gmail.com>
2024-10-07 11:31:35 -0300

    :wrench: chore: Initial commit

---
+++ src/main/java/dev/dluks/jsp03/DsJsp03Application.java
@@ -0,0 +1,13 @@
+package dev.dluks.jsp03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsJsp03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsJsp03Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dluks/jsp03/DsJsp03ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dluks.jsp03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsJsp03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
