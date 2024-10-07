116be6a7dedd5803398cc9d477b6dfb447ea4920
Diogo Oliveira <dluks82@gmail.com>
2024-10-07 11:36:58 -0300

    :sparkles: feat: Create `CustomErrorDTO` and `ValidationErrorDTO`

---
+++ src/main/java/dev/dluks/jsp03/dtos/ValidationErrorDTO.java
@@ -0,0 +1,30 @@
+package dev.dluks.jsp03.dtos;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationErrorDTO extends CustomErrorDTO {
+
+    private final List<FieldMessageDTO> errors = new ArrayList<>();
+
+    public ValidationErrorDTO() {
+    }
+
+    public ValidationErrorDTO(Instant timestamp, Integer status, String error, String path) {
+        super(timestamp, status, error, path);
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessageDTO(fieldName, message));
+    }
+
+    public List<FieldMessageDTO> getErrors() {
+        return errors;
+    }
+
+    public record FieldMessageDTO(String fieldName, String message) {
+
+    }
+
+}
