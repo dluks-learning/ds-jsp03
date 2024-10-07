3a6b364ec4ec33b33cd05a8dbd92d06d7749367e
Diogo Oliveira <dluks82@gmail.com>
2024-10-07 11:38:46 -0300

    :sparkles: feat: Add `ClientRepository`

---
+++ src/main/java/dev/dluks/jsp03/repositories/ClientRepository.java
@@ -0,0 +1,10 @@
+package dev.dluks.jsp03.repositories;
+
+import dev.dluks.jsp03.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
